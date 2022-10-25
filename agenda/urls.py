a8f1932a123d48c3b07b1928cc3d4e175a05c90a
Your Name <alunotal@email.com>
2022-10-25 16:47:17 -0300

    versao

--- agenda/urls.py
+++ agenda/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from home.views import index
+from django.contrib import admin
+from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('home.urls')),
 ]
--- home/views.py
+++ home/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+
+    context = {
+        'disciplina': 'Desenvolvimento web - Tecnico em informatica - politecnico - ufsm',
+        'tecnologia' : 'Python e django'
+    }
+    return render(request, 'index.html', context)
